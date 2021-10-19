5579fa7998ecb8112c104bda206223fbeeb15657
jan-2006 <73852449+jan-2006@users.noreply.github.com>
2021-10-19 19:04:57 +0200

    Create calculadorasimple.py

---
+++ calculadorasimple.py
@@ -0,0 +1,13 @@
+n1 = float(input("introdueix el primer nombre: ") )
+n2 = float(input("introdueix el segon nombre: ") )
+
+
+primero = n1 + n2
+segundo = n1 - n2
+tercero = n1 * n2
+cuarto = n1 / n2
+
+print ("la suma es", primero)
+print ("la resta es", segundo)
+print ("la multi es", tercero)
+print ("la divi es", cuarto)
